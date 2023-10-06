f731cd850921ed1788c791791783ff6cc1f751eb
TeePlunder <leon.bergmann23@gmail.com>
2023-10-06 13:31:12 +0200

    fix return and spaces

--- ascii.c
+++ ascii.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <ctype.h>
 
-void main(){
+int main(){
     int i,j;
-    for(i=0; i<4;i++) printf("|dev hex Char ");
+    for(i=0; i<4;i++) printf("|dec hex Char ");
     printf("|\n" );
     for (i=0; i < 128/4; i++) {
         printf("\n| ");
         for(j=0; j < 128; j += 128/4) {
             printf("%3d %2X ", j,j);
-            if(isgraph(j)) printf(" %c | ", j);
-            else printf(" . | ");
+            if(isgraph(j)) printf("  %c  | ", j);
+            else printf("  .  | ");
         }
     }
-
+    return 0;
 }
